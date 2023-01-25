38ea9aab56ce4d55203133c153bd013659989a84
WookyungYun <gyong1540@gmail.com>
2023-01-25 16:49:55 +0900

    ch03_06: 페이징 구현

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -17,22 +20,30 @@ function newsFeed() {
 
   newsList.push("<ul>");
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
   <li>
-    <a href="#${newsFeed[i].id}">
+    <a href="#/show/${newsFeed[i].id}">
       ${newsFeed[i].title} (${newsFeed[i].comments_count})
     </a>
   </li>
   `);
   }
-  newsList.push("</ul>");
 
+  newsList.push("</ul>");
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }">이전 페이지</a>
+      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+    </div>
+  `);
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  const id = location.hash.substr(1);
+  const id = location.hash.substring(7);
 
   const newsContents = getData(CONTENT_URL.replace("@id", id));
 
@@ -40,7 +51,7 @@ function newsDetail() {
     <h1>${newsContents.title}</h1>
 
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -48,6 +59,9 @@ function newsDetail() {
 function router() {
   const routePath = location.hash;
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
