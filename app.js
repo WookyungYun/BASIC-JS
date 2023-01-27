ade209f8c16d26f4a5f8a5f0203aeb9b401ab716
WookyungYun <gyong1540@gmail.com>
2023-01-27 17:21:14 +0900

    불필요한 연속적인 getData 호출방지

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const store = {
   currentPage: 1,
+  feeds: [],
 };
 
 function getData(url) {
@@ -15,7 +16,7 @@ function getData(url) {
 }
 
 function newsFeed() {
-  const newsFeed = getData(NEWS_URL);
+  let newsFeed = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -41,6 +42,10 @@ function newsFeed() {
       </div>
     </div>
   `;
+
+  if (newsFeed.length === 0) {
+    newsFeed = store.feeds = getData(NEWS_URL);
+  }
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
