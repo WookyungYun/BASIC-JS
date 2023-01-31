e627a914791ccbc0040946e618872ffeeaa2346e
WookyungYun <gyong1540@gmail.com>
2023-01-31 13:39:24 +0900

    ch03_09: 읽음 상태 표시

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
+function makeFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
+  return feeds;
+}
+
 function newsFeed() {
   let newsFeed = store.feeds;
   const newsList = [];
@@ -44,7 +51,7 @@ function newsFeed() {
   `;
 
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = getData(NEWS_URL);
+    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
   }
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -114,6 +121,14 @@ function newsDetail() {
     </div>
   </div>
 `;
+
+  for (let i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+
   function makeComment(comments, called = 0) {
     const commentString = [];
     for (let i = 0; i < comments.length; i++) {
