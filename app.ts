c753ea42bfd249892bfaa25af548f610080b5231
WookyungYun <gyong1540@gmail.com>
2023-02-16 13:02:47 +0900

    ch04_02: 변수에 타입 작성하기

--- app.ts
+++ app.ts
@@ -1,9 +1,23 @@
-const container = document.getElementById("root");
-const ajax = new XMLHttpRequest();
-const content = document.createElement("div");
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+};
+
+type NewsFeed = {
+  id: number;
+  comments_count: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean;
+};
+const container: HTMLElement | null = document.getElementById("root");
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
@@ -19,11 +33,20 @@ function makeFeeds(feeds) {
   for (let i = 0; i < feeds.length; i++) {
     feeds[i].read = false;
   }
+
   return feeds;
 }
 
+function updateView(html) {
+  if (container) {
+    container.innerHTML = html;
+  } else {
+    console.error("최상위 컨테이너가 없어 UI를 진행하지 못합니다.");
+  }
+}
+
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
     <div class="bg-gray-600 min-h-screen">
@@ -86,7 +109,7 @@ function newsFeed() {
   );
   template = template.replace("{{__next_page__}}", store.currentPage + 1);
 
-  container.innerHTML = template;
+  updateView(template);
 }
 
 function newsDetail() {
@@ -149,9 +172,8 @@ function newsDetail() {
 
     return commentString.join("");
   }
-  container.innerHTML = template.replace(
-    "{{__comments__}}",
-    makeComment(newsContents.comments)
+  updateView(
+    template.replace("{{__comments__}}", makeComment(newsContents.comments))
   );
 }
 
